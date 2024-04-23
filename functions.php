58adb6d2241ee9e06e2ba12f511208918133ee2d
BFTae <68056035+BFTae@users.noreply.github.com>
2024-04-23 12:25:57 +0200

    General structure
    require in all phps todo

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+function GetBanknoty(){
+    $plik=fopen("kasa.txt","r");
+    $linie=explode("\n",fread($plik,filesize("kasa.txt")));
+    $banknoty=[];
+    foreach ($linie as $key => $value) {
+        $value=explode(":",$value);
+        $banknoty[$value[0]]=$value[1];
+    }
+    fclose($plik);
+    return $banknoty;
+}
+function Wartosc($banknoty){
+    $wartosc=0;
+    foreach ($banknoty as $key => $value) {
+        $wartosc=intval($key)*intval($value);
+    }
+    return $wartosc;
+}
