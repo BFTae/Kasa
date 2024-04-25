0b75c5d4978189ccfaa8156031f100aa43e6f0b4
BFTae <68056035+BFTae@users.noreply.github.com>
2024-04-25 08:45:58 +0200

    Update wplata.php
    Just starting

--- wplata/wplata.php
+++ wplata/wplata.php
@@ -1,4 +1,36 @@
 <?php
+
+function GiveChange($money,$change){
+    foreach ($money as $key => $value) {
+        while ($value>0) {
+            if($change-floatval($key)<0){
+                break;
+            }
+            $change-=floatval($key);
+            $value-=1;
+            $money[$key]-=1;
+            $change=round($change,2);
+        }
+    }
+    print_r($money);
+    if ($change!=0) return $change;
+    
+    return 0;
+}
+
+function Handle($money,$given,$change){
+    $changeafter=GiveChange($money,$change);
+    if ($changeafter!=0){
+        echo "Nie będę miał jak wydać z $change zł, byłbym Panu winny ".strval($changeafter)."zł";
+    }
+
+}
 require dirname(__DIR__).'\functions.php';
 $money=GetMoney();
-print_r($money);
+
+
+$deposit=$_POST['kwota_dw'];
+
+$change=$_POST['change'];
+
+Handle($money,$deposit,$change);
