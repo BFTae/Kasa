b2c9a3d8b2ef9505ecbd3d7b8ef27c5beb4fc21c
BFTae <68056035+BFTae@users.noreply.github.com>
2024-04-25 10:35:49 +0200

    Finished wplata

---
+++ wplata/wplata/wplata.php
@@ -0,0 +1,62 @@
+<?php
+
+function GiveChange($money,$change){
+    $changenom=[];
+    foreach ($money as $key => $value) {
+        while ($value>0) {
+            if($change-floatval($key)<0){
+                break;
+            }
+            $change-=floatval($key);
+            $value-=1;
+            $money[$key]-=1;
+            if(isset($changenom[$key])==false){
+                $changenom[$key]=1;
+            }else{
+            $changenom[$key]+=1;
+            }
+            $change=round($change,2);
+        }
+    }
+    if ($change!=0) {
+        return $change;
+    }
+    SaveMoney($money);
+    return $changenom;
+}
+
+function DepositMoney($money,$deposit){
+    $cash=Chashify(0);
+    if(isset($_POST['ifbanknotami'])==false){
+        $cash=Chashify($deposit);
+    }else{
+        foreach ($cash as $key => $value) {
+            $cash[$key]=$_POST[str_replace(".","_",$key)];
+        }
+    }
+    return $cash;
+}
+
+function Handle($money,$deposit,$change){
+    $cash=DepositMoney($money,$deposit);
+    $moneypropose=Chashify(0);
+    foreach ($money as $key => $value) {
+        $moneypropose[$key]=$cash[$key]+$money[$key];
+    }
+    $changeafter=GiveChange($moneypropose,$change);
+    if (is_array($changeafter)==false){
+        echo "Nie będę miał jak wydać z $change zł";
+        return;
+    }
+    echo "Wydaję:<br>";
+    foreach ($changeafter as $key => $value) {
+        echo "$key zł: $value<br>";
+    }
+
+}
+require dirname(__DIR__).'\functions.php';
+$money=GetMoney();
+$deposit=$_POST['kwota_dw'];
+$change=$_POST['change'];
+
+Handle($money,$deposit,$change);
